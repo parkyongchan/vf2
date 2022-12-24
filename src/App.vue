af79109af3fefc319db1bdd1da5f4ef619da935b
parkyongchan <parkyongchan@naver.com>
2022-12-24 22:15:14 +0900

    202212242015

--- src/App.vue
+++ src/App.vue
@@ -5,35 +5,39 @@
       color="primary"
       dark
     >
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-toolbar-title>Page title</v-toolbar-title>
-    <v-spacer/>
-    <v-btn icon to="/about">
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-      <v-btn icon to="/">
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <site-title :title="title"></site-title>
+      <v-spacer/>
     </v-app-bar>
-<v-content>
+    <v-navigation-drawer app v-model="drawer">
+    
+      <v-divider></v-divider>
+      <site-menu></site-menu>
+    </v-navigation-drawer>
+    <v-content>
       <router-view/>
     </v-content>
-<v-footer app color ='primary' dark absolute>
-  <v-spacer></v-spacer>
-  <div>&copy;{{ new Date().getFullYear() }}</div>
-</v-footer>
+    <site-footer :footer="footer"></site-footer>
   </v-app>
 </template>
 
 <script>
-import router from './router';
-
+import SiteTitle from '@/views/site/title'
+import SiteFooter from '@/views/site/footer'
+import SiteMenu from '@/views/site/menu'
 
 export default {
-    name: "App",
-    data: () => ({
-    //
-    }),
-    components: { router }
+  components: { SiteTitle, SiteFooter, SiteMenu },
+  name: 'App',
+  data () {
+    return {
+      drawer: false,
+      items: [],
+      title: '나의 타이틀입니다',
+      footer: '푸터입니다'
+    }
+  },
+  mounted () {
+  }
 }
 </script>
---
+++ src/views/site/title.vue
@@ -0,0 +1,12 @@
+<template>
+
+<v-toolbar-title>
+    {{ title }}
+</v-toolbar-title>
+</template>
+
+<script>
+export default{
+    props:['title']
+}
+</script>
